f8ccc3b20560d1c2a7782f9cc6f9d3f358b025b2
Yana Kalytyn <ykalytyn@mifinity.com>
2024-04-24 15:16:26 +0100

    Added json server to the application

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -59,10 +59,12 @@ export class DetailsComponent {
   });
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
-
+  
   submitApplication() {
     this.housingService.submitApplication(
       this.applyForm.value.firstName ?? '',

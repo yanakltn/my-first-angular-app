f749f580bc74bd3330d2f0578b89a8873b7b23a7
Yana Kalytyn <ykalytyn@mifinity.com>
2024-04-24 13:17:04 +0100

    Added search for cities

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,13 +14,12 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
-  <section class="results">
-      <app-housing-location *ngFor="let housingLocation of housingLocationList"
-        [housingLocation]="housingLocation">
+    <section class="results">
+      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation">
       </app-housing-location>
   </section>
   `,
@@ -34,5 +33,18 @@ export class HomeComponent {
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+  
 }
